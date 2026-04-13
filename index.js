dbcb605817833fa3b6493b9e9480e1c7fd274d3f
Your Name <you@example.com>
2026-04-13 12:54:36 +0100

    new update

--- index.js
+++ index.js
@@ -1,53 +1,122 @@
 require('dotenv').config()
 
-const Person =require('./models/person.js')
+const Person = require('./models/person.js')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const cors =  require('cors')
-const mongoose = require('mongoose')
-
+const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-
-
 morgan.token('body', (req) => {
-    return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
 app.use(
-    morgan(':method :url :status :res[content-length] - :response-time ms :body')
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
 )
 // app.use(morgan('tiny'))
 // app.use(requestLogger)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-app.get('/api/persons', (req, res)=>{
-    Person.find({}).then(person=>{
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((person) => {
+    res.json(person)
+  })
+})
+
+app.get('/api/person/:id', (req, res) => {
+  Person.findByDd(req.params.id).then((note) => {
+    res.json(note)
+  })
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  if (!body) {
+    response.status(404).json({ error: 'name and number must be present' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  person
+    .save()
+    .then((savePerson) => {
+      response.json(savePerson)
+    })
+    .catch((error) => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
+  console.log(id)
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+
+  Person.findByIdAndUpdate(req.params.id)
+    .then((person) => {
+      if (!person) {
+        return res.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+      return person.save().then((updatePerson) => {
+        res.json(updatePerson)
+      })
+    })
+    .catch((error) => next(error))
+})
+
+app.get('/info', (req, res, next) => {
+  Person.countDocuments({})
+    .then((count) => {
+      const date = new Date()
+
+      res.send(`
+        <p>Phonebook has info for ${count} people</p>
+        <p>${date}</p>
+      `)
+    })
+    .catch((error) => next(error))
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
         res.json(person)
+      } else {
+        res.status(404).end()
+      }
     })
+    .catch((error) => next(error))
 })
 
-app.post('/api/persons', (req, res)=>{
-    const body = req.body
-    if(!body){
-        res.status(404).json({"error":"name and number must be present"})
-    }
-
-    const person= new Person({
-        name:body.name,
-        number:body.number
-    })
-    person.save().then(savePerson=>{
-        res.json(savePerson)
-    })
-})
-
-
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>{
-    console.log(`app is running  on  port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`app is running  on  port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-
 const url = process.env.MONGODB_URL
 console.log('connecting to', url)
 mongoose.set('strictQuery', false)
-mongoose.connect(url)
+mongoose
+  .connect(url)
   .then(() => {
     console.log('Connected to MongoDB')
   })
@@ -14,16 +14,36 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 4,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        // Regex explanation:
+        // ^           -> start
+        // \d{2,3}     -> 2 or 3 digits
+        // -           -> hyphen
+        // \d{5,}      -> at least 5 digits
+        // $           -> end
+        return /^\d{2,3}-\d{5,}$/.test(v)
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! Format must be XX-XXXXX or XXX-XXXXX`,
+    },
+  },
 })
-personSchema.set('toJSON',{
-  transform:(document, returnedObject)=>{
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id 
-     delete returnedObject.__v
-  }
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 })
 
 module.exports = mongoose.model('Person', personSchema)
-
